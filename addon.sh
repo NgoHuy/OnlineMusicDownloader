dd72a96c2960292d9712aa66af36cf9d5a81d607
NgoHuy <huynhok.uit@gmail.com>
2013-01-13 11:56:38 +0700

    script for addon 'open with' firefox

---
+++ addon.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo $1
+python2  /home/dragon/OnlineMusicDownloader/OnlineMusicDownloader.py -o /home/dragon/OnlineMusicDownloader/music.txt --download-with=wget -d . "$1"
+vlc --album-art 1 /home/dragon/OnlineMusicDownloader/music.txt
